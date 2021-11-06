e5140b6d7e727d472470ed61e2ce2958b94d2ea0
Wolfie <Wolfkid200444@gmail.com>
2021-11-05 21:09:44 -0400

    nothing so far has change

--- src/utils/getHeaders.ts
+++ src/utils/getHeaders.ts
@@ -13,5 +13,4 @@ export function getHeaders(this: any, { query, body }: any): any {
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
         Cookie: this.cookie,
       };
-
 }
